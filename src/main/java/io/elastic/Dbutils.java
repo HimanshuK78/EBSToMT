50f6dd224d34e6e98737a7a8ca4c02aed9d421da
unknown <himanshuexp001@gmail.com>
2018-12-03 21:23:32 +0530

    sd

--- src/main/java/io/elastic/Dbutils.java
+++ src/main/java/io/elastic/Dbutils.java
@@ -52,9 +52,10 @@ public class Dbutils {
 			 String username = usernameString.getString();
 			 String password = passwordString.getString();
      	    
-     	      */  
-		    // Class.forName("oracle.jdbc.driver.OracleDriver");	
-			 //logger.info("classes loaded successfully");   		
+			   */  
+			  logger.info("now OracleDriver will be loaded");  
+		      Class.forName("oracle.jdbc.driver.OracleDriver");	
+			  logger.info("classes loaded successfully");   		
      	   	 con = DriverManager.getConnection("jdbc:oracle:thin:@apps.example.com:1521:EBSDB","apps","apps");
     	   	 logger.info("Database Connection {} ok." + con);
     	   	
